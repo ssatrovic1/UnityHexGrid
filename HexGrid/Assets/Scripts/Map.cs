34296305007feb6fd09e5279d863472eec256067
Selmir Satrovic <s.selmir@hotmail.com>
2018-05-12 23:10:30 +0200

    dodani hexovi mapa moze se klikati

---
+++ HexGrid/Assets/Scripts/Map.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject hexPrefab;
+
+    //size of map
+    public int width;
+    public int height;
+
+    float xOffset = 0.882f;
+    float zOffset = 0.764f;
+
+	void Start () {
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                float xPos = i*xOffset;
+                if (j % 2 == 1)
+                    xPos += xOffset/2f;
+                
+                GameObject hex_go=(GameObject)Instantiate(hexPrefab, new Vector3(xPos, 0, j * zOffset), Quaternion.identity);
+                hex_go.name = "Hex_" + i + "_" + j;
+                hex_go.transform.SetParent(this.transform);
+                hex_go.isStatic = true;
+            }
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HexGrid/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+    GameObject pogodjeniHex;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray,out hitInfo))
+        {
+            //komad koda za hower preko hexa
+            /*if(pogodjeniHex==hitInfo.collider.transform.gameObject)
+            {
+                pogodjeniHex.GetComponentInChildren<MeshRenderer>().material.color = Color.red;
+            }
+            else if(pogodjeniHex!=null)
+            {
+                pogodjeniHex.GetComponentInChildren<MeshRenderer>().material.color = Color.white;
+            }*/
+            GameObject ourHitObject = hitInfo.collider.transform.gameObject;
+            
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                MeshRenderer mr = ourHitObject.GetComponentInChildren<MeshRenderer>();
+                if(mr.material.color==Color.red)
+                {
+                    mr.material.color = Color.white;
+                }
+                else
+                {
+                    mr.material.color = Color.red;
+                }
+                
+            }
+            //hitInfo.transform.GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("hittt  "+ourHitObject.name);
+            pogodjeniHex = ourHitObject;
+        }
+        
+
+    }
+}
